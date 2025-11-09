d394d6be08242176a6c78f6858becf21b6b3a70e
New Addict <121753715+raffxv@users.noreply.github.com>
2025-11-09 11:43:24 +0700

    Create WalletContextProvider.tsx

---
+++ src/lib/WalletContextProvider.tsx
@@ -0,0 +1,36 @@
+import React, { ReactNode, FC } from "react";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+
+import "@solana/wallet-adapter-react-ui/styles.css";
+
+// Define props for the provider
+interface WalletContextProviderProps {
+  children: ReactNode;
+}
+
+const WalletContextProvider: FC<WalletContextProviderProps> = ({ children }) => {
+  const endpoint =
+    "https://cool-clean-friday.solana-mainnet.quiknode.pro/5d4f1cb01a8c07492695c52553b7b7d525926a7f/";
+
+  // You can include more wallets here
+  const wallets = [new PhantomWalletAdapter(), new SolflareWalletAdapter()];
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>{children}</WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+};
+
+export default WalletContextProvider;
